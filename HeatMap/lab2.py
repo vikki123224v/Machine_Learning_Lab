f421d214c82245c1c9d37386cd7699c5000565f0
vikki123224v <vighneshwaras283@gmail.com>
2026-02-02 12:05:36 +0530

    Add files via upload

---
+++ HeatMap/lab2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_california_housing
+
+california_data=fetch_california_housing(as_frame=True)
+data=california_data.frame
+correltion_matrix=data.corr()
+plt.figure(figsize=(10,8))
+sns.heatmap(correltion_matrix,annot=True,cmap='BuPu',fmt='.2f',linewidths=1.5)
+plt.title('Correlation matrix of the california housing Features')
+plt.show()
+sns.pairplot(data,diag_kind='kde',plot_kws={'alpha':0.5})
+plt.suptitle('pair plot of california housing features ',y=1.02)
+plt.show()
