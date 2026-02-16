3bfb5d81ae8ff89462013907b73f92deb3ed0493
vikki123224v <vighneshwaras283@gmail.com>
2026-02-16 12:31:10 +0530

    Create find-s.py

---
+++ find-s.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+data=pd.read_csv('/content/enjoysport - Sheet1.csv')
+attribute=np.array(data)[:,:-1]
+target=np.array(data)[:,-1]
+print("Training data:\n")
+print(data)
+def train (att,tar):
+  for i,val in enumerate(tar):
+    if val=='yes':
+      specific_h=att[i].copy()
+      break
+  for i,val in enumerate(att):
+    if tar[i]=='yes':
+      for x in range(len(specific_h)):
+        if val[x]!=specific_h[x]:
+          specific_h[x]='?'
+        else:
+          pass
+  return specific_h
+print("\n")
+print(train(attribute,target))
